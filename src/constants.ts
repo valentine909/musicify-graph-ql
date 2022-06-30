f32ef4486e5a284974a508a3b458d85f2a7209ff
Valentine <valentine.nazarov@gmail.com>
2022-06-30 15:21:45 +0300

    feat: add endpoints constants

---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export enum Microservice {
+  albums = 'http://localhost:3005/v1/albums',
+  artists = 'http://localhost:3002/v1/artists',
+  bands = 'http://localhost:3003/v1/bands',
+  favourites = 'http://localhost:3007/v1/favourites',
+  genres = 'http://localhost:3001/v1/genres',
+  tracks = 'http://localhost:3006/v1/tracks',
+  users = 'http://localhost:3004/v1/users',
+}
